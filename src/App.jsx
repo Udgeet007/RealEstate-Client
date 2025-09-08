7cd8b0ff5bf99cd3d482bb85c3e0dd29184deb86
Udgeet <bhatt8udgeet@gmail.com>
2025-09-08 20:02:32 +0530

    Add routing pages implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home  from './Pages/Home';
+import About from "./Pages/About";
+import SignUp from './Pages/SignUp';
+import Profile from './Pages/Profile';
+import SignIn from './Pages/SignIn';
 
 function App() {
 
   return (
     <>
-    <h1 className='text-red-500 bg-amber-300'>Udgeet Bhatt</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
